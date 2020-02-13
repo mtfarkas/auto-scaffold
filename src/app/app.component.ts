48745381d5cc211884ba8c217f3075eec3620f2b
Farkas Máté <f.mate1128@gmail.com>
2020-02-13 16:31:54 +0100

    First iteration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,212 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { Clipboard } from '@angular/cdk/clipboard';
+import { ToastrService } from 'ngx-toastr';
+
+enum Tool {
+  PackageManager = 'pmc',
+  CLI = 'cli',
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'auto-scaffold';
+export class AppComponent implements OnInit, OnDestroy {
+  //#region Private fields
+  private subSink: Subscription = new Subscription();
+  //#endregion
+
+  //#region Public properties
+  fg: FormGroup;
+  generatedCommand = '';
+  //#endregion
+
+  //#region Ctor
+  constructor(private clipboard: Clipboard, private toastr: ToastrService) {}
+  //#endregion
+
+  //#region Lifecycle hooks
+  ngOnInit() {
+    this.fg = new FormGroup({
+      tool: new FormControl(Tool.PackageManager),
+      connectionString: new FormControl(''),
+      provider: new FormControl(''),
+      useAnnotations: new FormControl(false),
+      useDatabaseNames: new FormControl(false),
+      useForce: new FormControl(false),
+      contextName: new FormControl(''),
+      contextDir: new FormControl(''),
+      outputDir: new FormControl(''),
+      schemas: new FormControl(''),
+      tables: new FormControl(''),
+    });
+
+    this.subSink.add(this.fg.valueChanges.subscribe(_ => this.onFormChange()));
+
+    this.onFormChange();
+  }
+
+  ngOnDestroy() {
+    this.subSink.unsubscribe();
+  }
+  //#endregion
+
+  //#region Event handlers
+  onFormChange() {
+    if (this.fg.controls.tool.value === Tool.PackageManager) {
+      this.generatedCommand = this.getPMCString();
+    } else if (this.fg.controls.tool.value === Tool.CLI) {
+      this.generatedCommand = this.getCLIString();
+    }
+  }
+
+  onClearForm() {
+    this.fg.patchValue({
+      tool: Tool.PackageManager,
+      connectionString: '',
+      provider: '',
+      useAnnotations: false,
+      useDatabaseNames: false,
+      useForce: false,
+      contextName: '',
+      contextDir: '',
+      outputDir: '',
+      schemas: '',
+      tables: '',
+    });
+  }
+
+  onCopyToClipboard() {
+    const result = this.clipboard.copy(this.generatedCommand);
+
+    if (!result) {
+      this.toastr.success('Command successfully copied!');
+    } else {
+      this.toastr.error('Copying failed!');
+    }
+  }
+  //#endregion
+
+  //#region Helpers
+  private getPMCString(): string {
+    const controls = this.fg.controls;
+    const sb = ['Scaffold-DbContext'];
+
+    if (controls.connectionString.value) {
+      sb.push(`"${controls.connectionString.value}"`);
+    }
+
+    if (controls.provider.value) {
+      sb.push(controls.provider.value);
+    }
+
+    if (controls.outputDir.value) {
+      sb.push(`-OutputDir ${controls.outputDir.value}`);
+    }
+
+    if (controls.contextDir.value) {
+      sb.push(`-ContextDir ${controls.contextDir.value}`);
+    }
+
+    if (controls.contextName.value) {
+      sb.push(`-Context ${controls.contextName.value}`);
+    }
+
+    if (controls.schemas.value) {
+      const schemasString = (controls.schemas.value as string)
+        .split(',')
+        .map(val => `${val.trim()}`)
+        .filter(val => !!val)
+        .map(val => `"${val}"`)
+        .join(',');
+      sb.push(`-Schemas ${schemasString}`);
+    }
+
+    if (controls.tables.value) {
+      const tablesString = (controls.tables.value as string)
+        .split(',')
+        .map(val => `${val.trim()}`)
+        .filter(val => !!val)
+        .map(val => `"${val}"`)
+        .join(',');
+      sb.push(`-Tables ${tablesString}`);
+    }
+
+    if (controls.useAnnotations.value) {
+      sb.push('-DataAnnotations');
+    }
+
+    if (controls.useDatabaseNames.value) {
+      sb.push('-UseDatabaseNames');
+    }
+
+    if (controls.useForce.value) {
+      sb.push('-Force');
+    }
+
+    return sb.join(' ');
+  }
+
+  private getCLIString(): string {
+    const controls = this.fg.controls;
+    const sb = ['dotnet ef dbcontext scaffold'];
+
+    if (controls.connectionString.value) {
+      sb.push(`"${controls.connectionString.value}"`);
+    }
+
+    if (controls.provider.value) {
+      sb.push(controls.provider.value);
+    }
+
+    if (controls.outputDir.value) {
+      sb.push(`-o ${controls.outputDir.value}`);
+    }
+
+    if (controls.contextDir.value) {
+      sb.push(`--context-dir ${controls.contextDir.value}`);
+    }
+
+    if (controls.contextName.value) {
+      sb.push(`-c ${controls.contextName.value}`);
+    }
+
+    if (controls.schemas.value) {
+      const schemasString = (controls.schemas.value as string)
+        .split(',')
+        .map(val => `${val.trim()}`)
+        .filter(val => !!val)
+        .map(val => `"${val}"`)
+        .join(',');
+      sb.push(`--schema ${schemasString}`);
+    }
+
+    if (controls.tables.value) {
+      const tablesString = (controls.tables.value as string)
+        .split(',')
+        .map(val => `${val.trim()}`)
+        .filter(val => !!val)
+        .map(val => `"${val}"`)
+        .join(',');
+      sb.push(`-t ${tablesString}`);
+    }
+
+    if (controls.useAnnotations.value) {
+      sb.push('-d');
+    }
+
+    if (controls.useDatabaseNames.value) {
+      sb.push('--use-database-names');
+    }
+
+    if (controls.useForce.value) {
+      sb.push('-f');
+    }
+
+    return sb.join(' ');
+  }
+  //#endregion
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { ClipboardModule } from '@angular/cdk/clipboard';
+import { ToastrModule } from 'ngx-toastr';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormsModule,
+    ClipboardModule,
+    ToastrModule.forRoot({
+      timeOut: 3000,
+      preventDuplicates: true,
+      positionClass: 'toast-top-center',
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
